1cbf73d53f129b9c98e0be38d1a2ba3576d2b300
hyunu0831 <hyunu.dev@gmail.com>
2024-03-11 16:14:12 +0900

    feat: koa.js next()의 promise 반환 확인하기

--- src/index.js
+++ src/index.js
@@ -1,8 +1,30 @@
 const Koa = require('koa');
 const app = new Koa();
 
-app.use(ctx => { 
-    ctx.body = 'Hello World';
+app.use((ctx,next) => {
+    console.log(1);
+    next();
+})
+
+
+app.use((ctx,next)=> {
+    console.log(2);
+    next();
+})
+
+
+app.use((ctx,next) => {
+    ctx.body = 'Hello Koa';
+    next();
+})
+
+app.use((ctx, next) => { 
+    console.log(1);
+    const started = new Date();
+    next().then(() => {
+        const ms = new Date() - started;
+        console.log(`${ctx.method} ${ctx.url} - ${ms}ms`);
+    });
 })
 
 app.listen(4000, () => {
