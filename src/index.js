93b0fd2a07b4758b83b828d588c60b5a8104289c
hyunu0831 <hyunu.dev@gmail.com>
2024-03-11 16:29:12 +0900

    feat: routes(), allowedMethods 주석 추가

--- src/index.js
+++ src/index.js
@@ -8,7 +8,9 @@ router.get('/', (ctx, next) => {
     ctx.body = 'Hello Koa 서버입니다.';
 });
 
+//router.routes는 라우터 미들웨어를 앱에 적용합니다.
 app.use(router.routes());
+//allowedMethods는 허용되지 않은 메소드를 사용했을 때 405 Method Not Allowed를 응답합니다.
 app.use(router.allowedMethods());
 
 app.listen(4000, () => { 
