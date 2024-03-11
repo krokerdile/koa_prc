20d9324cb62f03ac2971f02aa0b5ecb9d331a64d
hyunu0831 <hyunu.dev@gmail.com>
2024-03-11 17:15:13 +0900

    feat: books api 따로 만들어서 사용

--- src/api/index.js
+++ src/api/index.js
@@ -1,9 +1,12 @@
 const Router = require('koa-router');
 
 const api = new Router();
+const books = require('./books');
 
 api.get('/books', (ctx, next) => {
     ctx.body = 'GET ' + ctx.request.path;
 });
+
+api.use('/books2',books.routes());
 
 module.exports = api;
