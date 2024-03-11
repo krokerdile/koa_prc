8007df098e1c8e8bfb0f8fb4217ed619540cc147
hyunu0831 <hyunu.dev@gmail.com>
2024-03-11 17:25:37 +0900

    feat: controller 나눠서 사용 - 모듈별 변수를 지정해서 사용 가능 - method를 기준으로 불러올 때 모듈별 변수를 선언하여서 원하는 파트 실행 가능

--- src/api/books/index.js
+++ src/api/books/index.js
@@ -2,8 +2,31 @@ const Router = require('koa-router');
 
 const books = new Router();
 
-books.get('/', (ctx, next) => {
-    ctx.body = 'GET ' + ctx.request.path;
-});
+// books.get('/', (ctx, next) => {
+//     ctx.body = `${ctx.request.method} ${ctx.request.path}`;
+// });
+
+//? 라우터가 어떤 메소드와 경로로 호출되었는지 확인하는 라우터
+// const handler = (ctx, next) => {
+//     ctx.body = `${ctx.request.method} ${ctx.request.path}`;
+// }
+
+//? books.controller를 불러온다
+const booksCtrl = require('./books.controller');
+
+// books.get('/', handler);
+books.get('/', booksCtrl.list);
+
+// books.post('/', handler);
+books.post('/', booksCtrl.create);
+
+// books.delete('/', handler);
+books.delete('/', booksCtrl.delete);    
+
+// books.put('/', handler);
+books.put('/', booksCtrl.replace);
+
+// books.patch('/', handler);
+books.patch('/', booksCtrl.update);
 
 module.exports = books;
