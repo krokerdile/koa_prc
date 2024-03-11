cf4fc465b545d13d301667d7e9715d7fbb7090c3
hyunu0831 <hyunu.dev@gmail.com>
2024-03-11 17:11:58 +0900

    feat: route 모듈화 - books api 따로 빼보기

--- src/index.js
+++ src/index.js
@@ -3,9 +3,30 @@ const Router = require('koa-router');
 
 const app = new Koa();
 const router = new Router();
+const api = require('./api');
+
+router.use('/api', api.routes()); 
 
 router.get('/', (ctx, next) => { 
     ctx.body = 'Hello Koa 서버입니다.';
+});
+
+router.get('/about', (ctx, next) => { 
+    ctx.body = '소개';
+});
+
+router.get('/about/:name', (ctx, next) => { 
+    const { name } = ctx.params;
+    ctx.body = name + '의 소개';
+});
+
+router.get('/posts', (ctx, next) => { 
+    const { id } = ctx.request.query;
+    if(id) {
+        ctx.body = '포스트 #' + id;
+    } else {
+        ctx.body = '포스트 아이디가 없습니다.';
+    }
 });
 
 //router.routes는 라우터 미들웨어를 앱에 적용합니다.
